379705922bf0dd78e75b39525b8bb4e612e61c97
mark <mark.appledeveloper@gmail.com>
2019-12-10 23:47:24 +0800

    Initial Commit

---
+++ BinanceAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BinanceAssignment
+//
+//  Created by 謝飛飛 on 2019/12/10.
+//  Copyright © 2019 MarkFly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
