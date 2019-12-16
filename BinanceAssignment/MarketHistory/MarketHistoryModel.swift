b072ac221a7dcce7fb445f227f16b22c5f8d2a6b
iOS_Mark <mark.hsieh@mh-tech.tw>
2019-12-16 17:54:39 +0800

    history model & cell

---
+++ BinanceAssignment/MarketHistory/MarketHistoryModel.swift
@@ -0,0 +1,37 @@
+//
+//  MarketHistoryModel.swift
+//  BinanceAssignment
+//
+//  Created by iOS_Mark on 2019/12/16.
+//  Copyright © 2019 MarkFly. All rights reserved.
+//
+
+import Foundation
+
+struct MarketHistory: Codable
+{
+    let eventType: String
+    let eventTime: Int
+    let symbol: String
+    let aggregateTradeID: Int
+    let price: String
+    let quantity: String
+    let firstTradeID: Int
+    let lastTradeID: Int
+    let tradeTime: Int
+    let isBuyer: Bool
+
+    enum CodingKeys: String, CodingKey
+    {
+        case eventType = "e"
+        case eventTime = "E"
+        case symbol = "s"
+        case aggregateTradeID = "a"
+        case price = "p"
+        case quantity = "q"
+        case firstTradeID = "f"
+        case lastTradeID = "l"
+        case tradeTime = "T"
+        case isBuyer = "m"
+    }
+}
---
+++ BinanceAssignment/MarketHistory/MarketHistoryTblCell.swift
@@ -0,0 +1,63 @@
+//
+//  MarketHistoryTblCell.swift
+//  BinanceAssignment
+//
+//  Created by iOS_Mark on 2019/12/16.
+//  Copyright © 2019 MarkFly. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class MarketHistoryTblCell: BATableViewCell
+{
+    let time = UILabel()
+    let price = UILabel()
+    let quantity = UILabel()
+    
+    required init?(coder: NSCoder)
+    {
+        super.init(coder: coder)
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?)
+    {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        layoutUI()
+    }
+    
+    func layoutUI()
+    {
+        contentView.addSubview(time)
+        time.textColor = .white
+        time.snp.makeConstraints
+        { (maker) in
+            maker.bottom.top.left.equalToSuperview()
+            maker.width.equalToSuperview().multipliedBy(0.25)
+        }
+        
+        contentView.addSubview(price)
+        price.snp.makeConstraints
+        { (maker) in
+            maker.bottom.top.equalToSuperview()
+            maker.left.equalTo(time.snp.right)
+            maker.width.equalToSuperview().multipliedBy(0.4)
+        }
+        
+        contentView.addSubview(quantity)
+        quantity.textColor = .white
+        quantity.snp.makeConstraints
+        { (maker) in
+            maker.bottom.top.right.equalToSuperview()
+            maker.left.equalTo(price.snp.right)
+        }
+    }
+    
+    func updateUI(history: MarketHistory)
+    {
+        time.text = "\(history.tradeTime)"
+        price.text = history.price
+        quantity.text = history.quantity
+        price.textColor = history.isBuyer ? UIColor.orderTextGreen : UIColor.orderTextRed
+    }
+}
